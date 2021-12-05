905c1e1cdf97b213a3941cc601cda94951d9c6f1
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-12-05 15:18:31 +0400

    Challenge instructions are added

---
+++ 4-ArrayCardio-Day1/script.js
@@ -0,0 +1,21 @@
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+
+// Array.prototype.map()
+// 2. Give us an array of the inventory first and last names
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to yongest
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+
+// 5. Sort the inventors by years lived
+
+// 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the name
+
+// 7. Sort Exercise
+// Sort the people alphaetically by last name
+
+// 8. Reduce exercise
+// Sum up the instances of each of these
