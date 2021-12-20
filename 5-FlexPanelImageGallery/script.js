75fcc5e01fc7d6d0107cef415b2d3dae519ec092
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-12-20 21:58:33 +0400

    By click transition of flex added JS

--- 5-FlexPanelImageGallery/script.js
+++ 5-FlexPanelImageGallery/script.js
@@ -1,16 +1,16 @@
 const panels = document.querySelectorAll('.panel')
 
-const toggleOpen = () => {
+const toggleOpen = (e) => {
   console.log("hello")
-  this.classList.toggle('open');
+  e.target.classList.toggle('open');
 }
 
 const toggleActive = (e) => {
   console.log(e.propertyName);
   if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
+    e.target.classList.toggle('open-active');
   }
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
