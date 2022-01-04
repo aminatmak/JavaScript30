d5a6655827683445704e7990a41c1f9732b78b07
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2022-01-04 20:15:26 +0400

    Canvas board to draw: By moving the mouse use the event information to draw on to canvas context using 'Mother-effing hsl'

--- 8-HTML5Canvas/script.js
+++ 8-HTML5Canvas/script.js
@@ -7,14 +7,18 @@ context.strokeStyle = '#9A0680';
 context.lineJoin = 'round';
 context.lineCap = 'round';
 context.lineWidth = 30;
+// context.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 const draw = (event) => {
   if (!isDrawing) return; //stop the function from running when they are not moused
   console.log(event);
+  context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   context.beginPath();
   // mouse start from
   context.moveTo(lastX, lastY);
@@ -22,11 +26,23 @@ const draw = (event) => {
   context.lineTo(event.offsetX, event.offsetY);
   context.stroke();
   [lastX, lastY] =  [event.offsetX, event.offsetY]
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (context.lineWidth >= 70 || context.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    context.lineWidth++;
+  } else {
+    context.lineWidth--;
+  }
 }
 canvas.addEventListener('mousedown', (event) => {
   isDrawing = true;
   [lastX, lastY] = [event.offsetX, event.offsetY]
-
 });
 
 canvas.addEventListener('mousemove', draw);
